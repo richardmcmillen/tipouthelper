76d708cb6d06c7799cf6eb2bc27cf402dedb3f06
Richard McMillen <ricky_mcmillen@yahoo.co.uk>
2013-01-27 03:33:43 -0800

    complete first revision of app. basic functionality in place.

--- app.rb
+++ app.rb
@@ -1,10 +1,39 @@
 require 'sinatra'
+require 'sinatra/session'
+
+set :session_fail, '/'
+set :session_secret, ENV['SESSION_SECRET'] || 'b1gs3cr3t'
 
 get '/' do
   erb :index
 end
 
-post '/' do
-  @names = params[:names].split(",")
-  erb :index
+post '/calculator' do
+  if params[:names]
+    session_start!
+    session[:tipsfor] = Hash.new
+    for name in params[:names].split(",")
+      session[:tipsfor][name] = []
+    end
+  end
+  if params[:amount] and params[:employee]
+    @each = params[:amount].to_f / params[:employee].length
+    session_start!
+    params[:employee].each_key { |employee| session[:tipsfor][employee].push(@each) }
+  end
+  erb :calculator
+end
+
+get '/calculator' do
+  session!
+  erb :calculator
+end
+
+get '/reset' do
+  session_end!
+  redirect '/'
+end
+
+not_found do
+  halt 404, 'Page Not Found Yo!'
 end
